a4fb52292c6208f4c674f8792bbc70c68b2f6046
olivmarcel <oliv.marcel@gmail.com>
2017-10-10 16:10:07 +0200

    Changement d'hébergement pour le static

---
+++ src/components/Rapport.js
@@ -0,0 +1,46 @@
+
+import React, { Component } from 'react';
+import { Button, Panel, PanelGroup, ResponsiveEmbed } from 'react-bootstrap';
+
+export default class Rapport extends Component {
+  
+  render() {
+    const pdf =(
+      <small className="text-muted"><b>Citation :</b> Marcel O., <i>Prototypage d'une interface WEB-SIG pour l'Observatoire Intégré des Risques Côtiers</i>, Master 2 Carthagéo : Université Panthéon-Sorbonne / Université Paris-Diderot / ENSG (Brest, 2017), 64p.
+      <Button 
+        bsSize="xsmall"
+        bsStyle="danger pull-right"  
+        className="glyphicon glyphicon-save-file"
+        href="http://localhost:3001/md/osi.pdf">
+        PDF</Button>
+      </small>
+      );
+
+    const panelGroupInstance = (
+      <PanelGroup defaultActiveKey="1" accordion>
+        <Panel 
+        header="Documentation sur le prototypage" 
+        footer={ pdf }
+        bsStyle="primary" 
+        eventKey="1">
+          <div style={{width: '100%', height: '100%'}}>
+            <ResponsiveEmbed a16by9>
+              <embed 
+              type="html" 
+              src="http://localhost:3001/md/osi.html" />
+            </ResponsiveEmbed>
+          </div>
+
+        </Panel>
+      </PanelGroup>
+    );
+
+    return(
+      <div>
+        
+        { panelGroupInstance }
+      </div>
+    )
+  }
+}
+
