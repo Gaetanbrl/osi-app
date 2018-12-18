78dbacc816d7a498706d61bdf5b7d1ca59361cc6
Yoann Verget <yverget@linaia.com>
2018-12-18 14:56:56 +0100

    Menu progress style 2

--- src/components/Indic.js
+++ src/components/Indic.js
@@ -20,50 +20,43 @@ render(){
 
     return(
 
-		<div>
+		<div className="submenu">
 			{this.props.niveau2.map(i2 => (
 			<div
-            id={i2.id}
-            key={i2.id}
-            activeKey={this.state.activeKey}
-            accordion
-            onSelect={this.handleSelect}>
-
+        id={i2.id}
+        key={i2.id}
+        activeKey={this.state.activeKey}
+        accordion
+        onSelect={this.handleSelect}>
 				<Panel
-				eventKey={i2.nom}
-				key={i2.nom}
-				bsStyle={bsCol[i2.composante]}>
-
-					<div>
-						<Panel.Title toggle>
-   						<div bsStyle={bsCol[i2.composante]}>
-   							{ i2.description }
-					        <Button
-					        	onClick={() => this.props.onClick(i2.id)}
-				        		className="pull-right"
-					        	bsStyle={bsCol[i2.composante]}
-								    disabled= {this.props.ableList.includes(i2.id)}
-					        	>
-						        <Glyphicon glyph="chevron-right"/>
-					        </Button>
-              </div>
-						</Panel.Title>
-
-					{this.props.niveau1.filter((i1) => (i1.thematique === i2.acronyme)).map(i1 => (
-   						 <Panel.Body key={i1.nom} collapsible>
-	   						<div
-							bsStyle={bsCol[i1.composante]}
-							onClick={() => this.props.onClick(i1.id)}
-							disabled= {!this.props.ableList.includes(i1.id)}
-							>
-
-								{i1.nom}
-								<small>{i1.description}</small>
-
-                            </div>
-						</Panel.Body>
-					))}
-					</div>
+  				eventKey={i2.nom}
+  				key={i2.nom}
+  				bsStyle={bsCol[i2.composante]}>
+					<Panel.Title toggle>
+      			{ i2.description }
+            <i class="far fa-angle-right"></i>
+            <Button
+            	onClick={() => this.props.onClick(i2.id)}
+          		className="pull-right"
+            	bsStyle={bsCol[i2.composante]}
+      		    disabled= {this.props.ableList.includes(i2.id)}
+            	>
+              <i class="far fa-angle-right"></i>
+            </Button>
+					</Panel.Title>
+          <div className="panel-list">
+  					{this.props.niveau1.filter((i1) => (i1.thematique === i2.acronyme)).map(i1 => (
+              <Panel.Body key={i1.nom} collapsible>
+                <div
+                className="submenu-link"
+                onClick={() => this.props.onClick(i1.id)}
+                disabled= {!this.props.ableList.includes(i1.id)}
+                >
+                  {i1.nom}
+                </div>
+              </Panel.Body>
+  					))}
+          </div>
 				</Panel>
 			</div>
 				))}
